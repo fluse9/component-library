78e61495bfc15f681b3d9f795e2bf8344dab6517
fluse9 <fluse9@gmail.com>
2023-05-28 23:36:05 -0500

    fix(develop): Fixed linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,45 @@
 module.exports = {
-  extends: ['airbnb-typescript', 'plugin:react/recommended', 'prettier', 'plugin:storybook/recommended'],
-  plugins: ['prettier'],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  env: {
-    browser: true,
-    node: true
-  },
-  rules: {
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-process-exit': 'off',
-    'object-shorthand': 'off',
-    'class-methods-use-this': 'off',
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.tsx']
-    }],
-    'react/function-component-definition': [2, {
-      namedComponents: 'arrow-function',
-      unnamedComponents: 'arrow-function'
-    }]
-  }
+    extends: [
+        'airbnb-typescript',
+        'plugin:react/recommended',
+        'prettier',
+        'plugin:storybook/recommended',
+    ],
+    plugins: ['prettier', 'import'],
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-unused-vars': 'warn',
+        'no-console': 'off',
+        'func-names': 'off',
+        'no-process-exit': 'off',
+        'object-shorthand': 'off',
+        'class-methods-use-this': 'off',
+        'react/jsx-filename-extension': [
+            1,
+            {
+                extensions: ['.tsx'],
+            },
+        ],
+        'react/function-component-definition': [
+            2,
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
