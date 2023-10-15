426759b8405bce09bca27556822b2ac16a01115a
fluse9 <fluse9@gmail.com>
2023-10-15 18:22:14 -0500

    fix(develop): Created temp folders for components and types

--- src/com/index.tsx
+++ src/com/index.tsx
@@ -1,4 +1,4 @@
 export { default as Anchor } from './Anchor';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
-export { default as Typography } from './Typography';
+export { default as Typography } from './typ';
